ee2646bcbdee8a8e939f9d8bd2c5a719b785306e
Muhammad Haikal Hakim <haikalhakim530@gmail.com>
2026-06-23 17:34:49 +0700

    add: 01_basics - hello world

---
+++ 01_hello_world.py
@@ -0,0 +1,11 @@
+# print() adalah fungsi untuk menampilkan output ke layar
+# Ini adalah program pertama yang biasa ditulis semua programmer ketika belajar bahasa pemrograman baru
+
+print("Hello, World!")
+# Output: Hello, World!
+
+print("I am learning Python.")
+# Output: I am learning Python.
+
+print("Python is fun!")
+# Output: Python is fun!
